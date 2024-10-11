49ba2f1fe97027048e6cebffade3a326937e9df9
Soufyane Marsi <smarsi@c4r3p2.1337.ma>
2024-10-11 21:06:14 +0100

    Initialize Zombie class

---
+++ C++ - Module 01/ex00/Zombie.hpp
@@ -0,0 +1,14 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+#include <iostream>
+#include <iomanip>
+
+class	Zombie
+{
+	private :
+		std::string	name;
+	public :
+		void announce( void );
+};
+
+#endif

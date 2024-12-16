78b07969a6ede31d42d68b50dfc2f9e8783d5fd0
Soufyane Marsi <smarsi@c4r9p10.1337.ma>
2024-12-16 11:42:23 +0100

    CPP02 DONE

--- C++ - Module 02/ex00/Fixed.hpp
+++ C++ - Module 02/ex00/Fixed.hpp
@@ -1,19 +1,15 @@
-#ifndef FIXED_HPP
-#define FIXED_HPP
-
+#pragma once
 #include <iostream>
 
 class Fixed
 {
-    int	 fixed_point;
-	static const int fractional = 8;
+    int					fixed_point;
+	static const int	fractional = 8;
 	public :
 		Fixed();
-		Fixed(const Fixed& obj);
-		Fixed& operator=(const Fixed& obj);
-		int		getRawBits( void ) const;
-		void	setRawBits( int const raw );
+		Fixed(const Fixed &obj);
+		Fixed &operator=(const Fixed &obj);
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
 		~Fixed();
 };
-
-#endif
--- C++ - Module 02/ex01/Fixed.cpp
+++ C++ - Module 02/ex01/Fixed.cpp
@@ -4,11 +4,22 @@ Fixed::Fixed() : fixed_point(0)
 {
     std::cout << "Default constructor called" << std::endl;
 }
-
 Fixed::Fixed(const Fixed &obj)
 {
     std::cout << "Copy constructor called" << std::endl;
     *this = obj;
+}
+
+// ------------------ parametrized constructor :
+
+Fixed::Fixed(const int _fixed_point)
+{
+    fixed_point = _fixed_point << fractional;
+}
+
+Fixed::Fixed(const float _fixed_point)
+{
+    fixed_point = roundf( _fixed_point * (1 << fractional) );
 }
 
 Fixed &Fixed::operator=(const Fixed &obj)
@@ -19,16 +30,35 @@ Fixed &Fixed::operator=(const Fixed &obj)
     return (*this);
 }
 
+// ------------------ toInt and toFLoat 
+
+float	Fixed::toFloat( void ) const
+{
+    return ( (float)fixed_point / (1 << fractional) );
+}
+
+int		Fixed::toInt( void ) const
+{
+    return ( fixed_point >> fractional );
+}
+
 int     Fixed::getRawBits( void ) const
 {
     std::cout << "getRawBits member function called" << std::endl;
-    return (fixed_point);
+    return ( fixed_point );
 }
 
 void    Fixed::setRawBits( int const raw )
 {
     fixed_point = raw;
     std::cout << "setRawBits member function called" << std::endl;
+}
+// -------------------------- overload of the insertion («)
+
+std::ostream&	operator<<( std::ostream& cout, const Fixed &obj )
+{
+    cout << obj.toFloat();
+    return (cout);
 }
 
 Fixed::~Fixed()
